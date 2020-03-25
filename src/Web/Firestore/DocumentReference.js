5c50e69012be0d8a3335cf767dfc491c115b97e3
Marco Perone <pasafama@gmail.com>
2020-03-25 11:09:11 +0100

    fix comparison between document references

--- src/Web/Firestore/DocumentReference.js
+++ src/Web/Firestore/DocumentReference.js
@@ -1,12 +1,22 @@
 "use strict";
 
+const lodash = require('lodash')
+
+const docRefPath = function (docRef) {
+  const segments = docRef._key.path.segments
+  const offset   = docRef._key.path.offset
+  const length   = docRef._key.path.len
+
+  return segments.slice(offset, offset + length)
+}
+
 exports.eqImpl = function (docRef1, docRef2) {
-  return docRef1._key.path === docRef2._key.path
+  return lodash.isEqual(docRefPath(docRef1), docRefPath(docRef2))
 }
 
 exports.showImpl = function (docRef) {
   const ret = {
-    path: docRef._key.path.segments
+    path: docRef._key.path
   }
 
   return JSON.stringify(ret)
